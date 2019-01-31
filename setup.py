2b8ab05474a65df01cf629e1400c863b29b00078
@japzio <jasper.culong@trustarc.com>
2019-01-31 18:43:20 +0800

    setup

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@
 from distutils.core import setup
 
 setup(
-  name='ecr-image-copy-py',
-  version='1.0',
-  description='aws ecr docker image cross-region replicator',
-  author='Jasper Culong',
-  author_email='jculongit10@yahoo.com',
-  packages=['tests'],
-  install_requires=[
-    'boto3',
-    'docker',
-  ]
+    name='ECR Image Copy',
+    version='0.1.0',
+    author='Jasper Culong',
+    author_email='jasper.culong@truste.com',
+    packages=['tests'],
+    license='LICENSE.txt',
+    description='Useful towel-related stuff.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "docker >= 3.7.0",
+        "boto3 >= 1.7.57 ",
+    ],
 )
