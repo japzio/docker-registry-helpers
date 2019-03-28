ce0a88d9346a72aa1d63a905637337da2ee610eb
@japzio <jasper.culong@trustarc.com>
2019-03-28 19:24:04 +0800

    added setup tools

--- README.md
+++ README.md
@@ -4,21 +4,28 @@ AWS ECR docker image cross-region replicator
 
 ## Requirements
 
-* python3 and pip3
+* python3
 * boto3
 * docker-py
+
+### Installation 
+
+~~~
+pip3 install ecr2ecr==1.2
+~~~
 
 ### Usage
 
 ~~~
-./main.py -s us-west-2 -d us-east-1 -n image-name -t latest
+ecr2ecr -s us-west-2 -d us-east-1 -n image-name -t latest
 ~~~
 
-### Parameters
+### Arguments
 
-~~~
-./main.py --help
-~~~
+*  -s SOURCE      --source-region      ecr region where the image should be pulled from.
+*  -d DESTINATION --destination-region ecr region where the image will be pushed to.
+*  -n IMAGE_NAME  --image-name         ecr image:tag format
+*  -t IMAGE_TAG   --image-tag          ecr image:tag format
 
 ## Author
 
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3 
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='ECR Image Copy',
-    version='1.0',
+    name='ecr2ecr',
+    version='1.2',
     author='Jasper Culong',
-    author_email='jasper.culong@truste.com',
-    packages=['tests'],
+    author_email='jculongit10@yahoo.com',
+    packages=find_packages(),
     license='LICENSE.txt',
     description='Useful towel-related stuff.',
     long_description=open('README.md').read(),
+    url="https://pypi.org/project/ecr2ecr",
+    entry_points={
+        'console_scripts': [
+            'ecr2ecr = ecr2ecr.core:main'
+        ]
+    },
     install_requires=[
         "docker >= 3.7.0",
         "boto3 >= 1.7.57",
