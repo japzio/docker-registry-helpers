7d357f28dc9164d0c85bc4b11856d74b3cec684d
@japzio <jasper.culong@trustarc.com>
2019-04-03 11:59:01 +0800

    bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecr2ecr',
-    version='1.14',
+    version='1.15',
     author='Jasper Culong',
     author_email='jculongit10@yahoo.com',
     packages=find_packages(),
