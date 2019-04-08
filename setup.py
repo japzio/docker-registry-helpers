5f430de6467016959d0c74b3e7bed076263cd119
@japzio <jasper.culong@trustarc.com>
2019-04-08 11:40:44 +0800

    modified tox,setup; added VERSION file

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3 
 
 from setuptools import setup, find_packages
+import os
+
+with open('{}/{}'.format(os.path.abspath('.'),'VERSION'), 'r') as version_file:
+  VERSION = version_file.read().strip()
 
 setup(
-    name='ecr2ecr',
-    version='1.18',
-    author='Jasper Culong',
-    author_email='jculongit10@yahoo.com',
-    packages=find_packages(),
-    license='LICENSE',
-    description='docker image registry sync tools. Currently supports aws ecr.',
-    long_description=open('README.md').read(),
-    url="https://pypi.org/project/ecr2ecr",
-    entry_points={
-        'console_scripts': [
-            'ecr2ecr = ecr2ecr.core:main'
-        ]
-    },
-    install_requires=[
-        "docker-py <= 1.10.6",
-        "boto3 >= 1.7.57",
-        "twine == 1.13.0",
-        "unittest2"
-    ],
+  name='ecr2ecr',
+  version=VERSION,
+  author='Jasper Culong',
+  author_email='jculongit10@yahoo.com',
+  packages=find_packages(),
+  license='LICENSE',
+  description='docker image registry sync tools. Currently supports aws ecr.',
+  long_description=open('README.md').read(),
+  url="https://pypi.org/project/ecr2ecr",
+  entry_points={
+    'console_scripts': [
+        'ecr2ecr = ecr2ecr.core:main'
+    ]
+  },
+  install_requires=[
+    "docker-py <= 1.10.6",
+    "boto3 >= 1.7.57",
+    "twine == 1.13.0",
+    "unittest2"
+  ],
 )
